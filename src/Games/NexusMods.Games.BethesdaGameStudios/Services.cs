cac0e1bfcd2ad640ee58dabc8870f4e82689f161
Mia <miathewiccancatgirl@gmail.com>
2024-06-17 19:54:08 -0400

    add Fallout 4

--- src/Games/NexusMods.Games.BethesdaGameStudios/Services.cs
+++ src/Games/NexusMods.Games.BethesdaGameStudios/Services.cs
@@ -6,6 +6,7 @@ using NexusMods.Abstractions.Serialization.ExpressionGenerator;
 using NexusMods.Extensions.DependencyInjection;
 using NexusMods.Games.BethesdaGameStudios.SkyrimLegendaryEdition;
 using NexusMods.Games.BethesdaGameStudios.SkyrimSpecialEdition;
+using NexusMods.Games.BethesdaGameStudios.Fallout4;
 
 namespace NexusMods.Games.BethesdaGameStudios;
 
@@ -14,8 +15,10 @@ public static class Services
     public static IServiceCollection AddBethesdaGameStudios(this IServiceCollection services) =>
         services.AddGame<SkyrimSpecialEdition.SkyrimSpecialEdition>()
             .AddGame<SkyrimLegendaryEdition.SkyrimLegendaryEdition>()
+            .AddGame<Fallout4.Fallout4>()
             .AddSingleton<ITool, SkyrimLegendaryEditionGameTool>()
             .AddSingleton<ITool, SkyrimSpecialEditionGameTool>()
+            .AddSingleton<ITool, Fallout4GameTool>()
             .AddGeneratedFile<PluginOrderFile>()
             .AddSingleton<PluginAnalyzer>()
             .AddSingleton<PluginSorter>();
